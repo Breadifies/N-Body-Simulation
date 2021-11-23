1fd31b79646f24db089eae75fcd0ea0f7fb83053
Breadifies <87525087+Breadifies@users.noreply.github.com>
2021-11-23 12:14:49 +0000

    Update buttonEventHandling.js

--- buttonEventHandling.js
+++ buttonEventHandling.js
@@ -1,3 +1,30 @@
+
+///////////////////////////////////////////// OBJECT BOTTOM
+////////////////////////////////////////////
+
+
+function showBtnBotObj() {//displays or removes the relevant div element using a button 
+  let x = document.querySelector("#objectBotChild");
+  if (x.style.display === "none") {//changes the CSS style of the block depending on what it is currently
+    x.style.display = "block";
+  }else{
+    x.style.display = "none";
+  }
+}
+document.querySelector("#massChange").value = 0.005;
+document.querySelector("#updateMass").addEventListener("click", function(){
+  dragMass = document.querySelector("#massChange").value;
+});
+
+document.querySelector("#sizeChange").value = 5;
+document.querySelector("#updateSize").addEventListener("click", function(){
+  dragSize = document.querySelector("#sizeChange").value;
+});
+
+///////////////////////////////////////////// PHYSICS MANIPULATION
+////////////////////////////////////////////
+
+
 function showBtnPhysics() {//displays or removes the relevant div element using a button 
     let x = document.querySelector("#physicsChild");
     if (x.style.display === "none") {//changes the CSS style of the block depending on what it is currently
@@ -6,17 +33,56 @@ function showBtnPhysics() {//displays or removes the relevant div element using 
       x.style.display = "none";
     }
   }
-
-
-
-
-document.querySelector("#ugcChange").value = nBodyInstance.UGC;
-
+document.querySelector("#ugcChange").value = nBodyInstance.UGC; //sets textbox value to that of default
 document.querySelector("#updateUGC").addEventListener("click",function() {
+  if ((document.querySelector("#ugcChange").value) >= 750){
+    document.querySelector("#ugcChange").value = 750;
+  }else if ((document.querySelector("#ugcChange").value) <= -750){
+    document.querySelector("#ugcChange").value = -750;
+  }
+  //validation check for in range
   nBodyInstance.UGC = document.querySelector("#ugcChange").value;
   //replaces the value of UGC with what is in the textbox
 });
 
-function slideUpdate(value) {
+function slideUpdateUGC(value) { //updates textbox value based on slider value
   document.querySelector("#ugcChange").value = value;
 }
+
+
+/////////////////////////////////////////////// TIME DILATION
+//////////////////////////////////////////////
+
+function showBtnTime() {
+  let x = document.querySelector("#timeChild");
+  if (x.style.display ==="none") {
+    x.style.display = "block";
+  }else{
+    x.style.display = "none";
+  }
+}
+document.querySelector("#timeChange").value = 1;
+document.querySelector("#updateTime").addEventListener("click", function()
+{
+  if ((document.querySelector("#timeChange").value) >= 10){
+    document.querySelector("#timeChange").value = 10;
+  }else if ((document.querySelector("#timeChange").value) <= -1){
+    document.querySelector("#timeChange").value = -1;
+  }
+  nBodyInstance.dt = document.querySelector("#timeChange").value * dt;
+   //normalises scale for time relative to the original time
+});
+function slideUpdateTime(value) {
+  document.querySelector("#timeChange").value = value;
+}
+
+//////////////////////////////////////////CLEAR SIMULATION
+//////////////////////////////////////////
+
+function clearSim() {
+  nBodyInstance.cBodies = []; 
+  //clears array instance by assigning it an empty array
+}
+
+/////////////////////////////////////////
+/////////////////////////////////////////
